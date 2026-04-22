1e79d78c8e8e522cb40e130694d0a02157da383c
BalkundeAkash <akashbalkunde@gmail.com>
2026-04-23 00:04:49 +0530

    Basics_01 java 25

---
+++ Basics_01/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+
+    public static void main(String[] myArgs) {
+        System.out.println("Hello World!!!");
+    }
+
+    public static void main(String[] myArgs, float num){
+        System.out.println("Hello World!!!");
+    }
+
+    public double main(){
+        return 3.14;
+    }
+
+}
